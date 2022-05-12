f04b90cceade17f39f76aef158dcaea8f71e9c9c
jshlbrd <jliburdi@brex.com>
2022-05-12 04:56:44 +0000

    chore: add pypi distro

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="py-aggregate",
+    version="0.1.1",
+    license="MIT",
+    author="Josh Liburdi",
+    author_email="liburdi.joshua@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/jshlbrd/py-aggregate",
+)
